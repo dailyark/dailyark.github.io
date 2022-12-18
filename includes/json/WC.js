d7c5486120be53d641617acd87189b1d038702d6
lorddusk <lang2619@gmail.com>
2022-12-18 16:55:02 +0100

    Updated to latest version of the game.

--- includes/json/WC.js
+++ includes/json/WC.js
@@ -1,107 +1,114 @@
 var weeklychar = {
-    "abyss-dungeon": {
-        task: "Abyss Dungeon",
-        url: "https://papunika.com/abyss-dungeons/",
-        short: true,
-        desc: "Once per week per Abyss Dungeon.</br>You can only complete one difficulty per week.",
-        img: "abyss dungeon"
-    },
-    "una-weekly": {
-        task: "Una Weekly 1",
-        url: "https://papunika.com/unas-tasks-overview/",
-        short: true,
-        desc: "3 Una tasks per week.</br>Can be expanded up to +1.",
-        img: "una weekly"
-    },
-    "una-weekly-2": {
-        task: "Una Weekly 2",
-        url: "https://papunika.com/unas-tasks-overview/",
-        short: true,
-        desc: "3 Una tasks per week.</br>Can be expanded up to +1.",
-        img: "una weekly"
-    },
-    "una-weekly-3": {
-        task: "Una Weekly 3",
-        url: "https://papunika.com/unas-tasks-overview/",
-        short: true,
-        desc: "3 Una tasks per week.</br>Can be expanded up to +1.",
-        img: "una weekly"
-    },
-    "guild-weekly": {
-        task: "Guild Weekly 1",
-        url: "#",
-        short: true,
-        desc: "Depends what the guild officers pick.",
-        img: "guild research"
-    },
-        "guild-weekly-2": {
-        task: "Guild Weekly 2",
-        url: "#",
-        short: true,
-        desc: "Depends what the guild officers pick.",
-        img: "guild research"
-    },
-        "guild-weekly-3": {
-        task: "Guild Weekly 3",
-        url: "#",
-        short: true,
-        desc: "Depends what the guild officers pick.",
-        img: "guild research"
-    },
-    "abyss-raid": {
-        task: "Abyss Raid",
-        url: "https://papunika.com/abyss-raids/",
-        short: true,
-        desc: "1 per week per Abyss Raid.",
-        img: "abyssal"
-    },
-    "valtan-legion-raid": {
-        task: "Valtan Legion Raid",
-        url: "https://papunika.com/legion-raids/",
-        short: true,
-        desc: "1 per week (shared between difficulties).",
-        img: "legion"
-    },
-    "vykas-legion-raid": {
-        task: "Vykas Legion Raid",
-        url: "https://papunika.com/legion-raids/",
-        short: true,
-        desc: "1 per week (shared between difficulties).",
-        img: "legion"
-    },
-    "kakul-saydon-legion-raid": {
-        task: "Kakul Saydon Legion Raid",
-        url: "https://papunika.com/legion-raids/",
-        short: true,
-        desc: "1 per week (shared between difficulties).",
-        img: "legion"
-    },
-    "g-v-e": {
-        task: "Raid Match",
-        url: "https://lost-ark.maxroll.gg/resources/gvg-islands-guide",
-        short: true,
-        desc: "Once per week challenge (Raid Match) to kill a raid boss for Guild Contribution and Bloodstones.",
-        img: "sylmael bloodstone"
-    },
-    "merchant-ship-exchange": {
-        task: "Merchant Ship Exchange",
-        url: "#",
-        short: true,
-        desc: "Supply replenishes on weekly reset.",
-        img: "merchant"
-    },
-    "silmael-bloodstone-exchange": {
-        task: "Silmael Bloodstone Exchange",
-        url: "#",
-        short: true,
-        desc: "Supply replenishes on weekly reset.",
-        img: "sylmael bloodstone"
-    },
-    "chaos-dungeon-shard-exchange": {
-        task: "Chaos Dungeon Shard Exchange",
-        url: "#",
-        short: true,
-        desc: "Supply replenishes on weekly reset.",
-        img: "chaos dungeon"
-    }
+   "abyss-dungeon":{
+      "task":"Abyss Dungeon",
+      "url":"https://papunika.com/abyss-dungeons/",
+      "short":true,
+      "desc":"Once per week per Abyss Dungeon.</br>You can only complete one difficulty per week.",
+      "img":"abyss dungeon"
+   },
+   "una-weekly":{
+      "task":"Una Weekly 1",
+      "url":"https://papunika.com/unas-tasks-overview/",
+      "short":true,
+      "desc":"3 Una tasks per week.</br>Can be expanded up to +1.",
+      "img":"una weekly"
+   },
+   "una-weekly-2":{
+      "task":"Una Weekly 2",
+      "url":"https://papunika.com/unas-tasks-overview/",
+      "short":true,
+      "desc":"3 Una tasks per week.</br>Can be expanded up to +1.",
+      "img":"una weekly"
+   },
+   "una-weekly-3":{
+      "task":"Una Weekly 3",
+      "url":"https://papunika.com/unas-tasks-overview/",
+      "short":true,
+      "desc":"3 Una tasks per week.</br>Can be expanded up to +1.",
+      "img":"una weekly"
+   },
+   "guild-weekly":{
+      "task":"Guild Weekly 1",
+      "url":"#",
+      "short":true,
+      "desc":"Depends what the guild officers pick.",
+      "img":"guild research"
+   },
+   "guild-weekly-2":{
+      "task":"Guild Weekly 2",
+      "url":"#",
+      "short":true,
+      "desc":"Depends what the guild officers pick.",
+      "img":"guild research"
+   },
+   "guild-weekly-3":{
+      "task":"Guild Weekly 3",
+      "url":"#",
+      "short":true,
+      "desc":"Depends what the guild officers pick.",
+      "img":"guild research"
+   },
+   "abyss-raid":{
+      "task":"Abyss Raid",
+      "url":"https://papunika.com/abyss-raids/",
+      "short":true,
+      "desc":"1 per week per Abyss Raid.",
+      "img":"abyssal"
+   },
+   "valtan-legion-raid":{
+      "task":"Valtan Legion Raid",
+      "url":"https://papunika.com/legion-raids/",
+      "short":true,
+      "desc":"1 per week (shared between difficulties).",
+      "img":"legion"
+   },
+   "vykas-legion-raid":{
+      "task":"Vykas Legion Raid",
+      "url":"https://papunika.com/legion-raids/",
+      "short":true,
+      "desc":"1 per week (shared between difficulties).",
+      "img":"legion"
+   },
+   "kakul-saydon-legion-raid":{
+      "task":"Kakul Saydon Legion Raid",
+      "url":"https://papunika.com/legion-raids/",
+      "short":true,
+      "desc":"1 per week (shared between difficulties).",
+      "img":"legion"
+   },
+   "Brelshaza Legion Raid":{
+      "task":"Brelshaza Legion Raid",
+      "url":"https://maxroll.gg/lost-ark/legion-raids/brelshaza-gate-1",
+      "short":true,
+      "desc":"Brelshaza is the fourth Legion Raid in Lost Ark.",
+      "img":"legion.png"
+   },
+   "g-v-e":{
+      "task":"Raid Match",
+      "url":"https://lost-ark.maxroll.gg/resources/gvg-islands-guide",
+      "short":true,
+      "desc":"Once per week challenge (Raid Match) to kill a raid boss for Guild Contribution and Bloodstones.",
+      "img":"sylmael bloodstone"
+   },
+   "merchant-ship-exchange":{
+      "task":"Merchant Ship Exchange",
+      "url":"#",
+      "short":true,
+      "desc":"Supply replenishes on weekly reset.",
+      "img":"merchant"
+   },
+   "silmael-bloodstone-exchange":{
+      "task":"Silmael Bloodstone Exchange",
+      "url":"#",
+      "short":true,
+      "desc":"Supply replenishes on weekly reset.",
+      "img":"sylmael bloodstone"
+   },
+   "chaos-dungeon-shard-exchange":{
+      "task":"Chaos Dungeon Shard Exchange",
+      "url":"#",
+      "short":true,
+      "desc":"Supply replenishes on weekly reset.",
+      "img":"chaos dungeon"
+   }
 }
--- includes/json/WR.js
+++ includes/json/WR.js
@@ -1,82 +1,75 @@
 var weeklies = {
-    "guardian-challenge-mode": {
-        task: "Guardian Challenge Mode 1",
-        url: "https://papunika.com/guardian-subjugations/",
-        desc: "1 per week per Boss (3 in total).",
-        img: "guardian raid"
-    },
-    "guardian-challenge-mode-2": {
-        task: "Guardian Challenge Mode 2",
-        url: "https://papunika.com/guardian-subjugations/",
-        desc: "1 per week per Boss (3 in total).",
-        img: "guardian raid"
-    },
-    "guardian-challenge-mode-3": {
-        task: "Guardian Challenge Mode 3",
-        url: "https://papunika.com/guardian-subjugations/",
-        desc: "1 per week per Boss (3 in total).",
-        img: "guardian raid"
-    },
-    "challenge-abyss-dungeon": {
-        task: "Challenge Abyss Dungeon",
-        url: "https://papunika.com/abyss-dungeons/",
-        short: true,
-        desc: "1 per week per Challenge Abyss Dungeon.",
-        img: "chal abyss dungeon"
-    },
-    "gvg-guild-boss": {
-        task: "GVG / Guild Boss",
-        url: "#",
-        short: true,
-        desc: "Once per week.",
-        img: "guild"
-    },
-    "pvp-token-exchange": {
-        task: "PVP Token Exchange",
-        url: "#",
-        short: true,
-        desc: "Supply replenishes on weekly reset.",
-        img: "pvp token"
-    },
-    "stronghold-activities": {
-        task: "Stronghold Activities",
-        url: "#",
-        short: true,
-        desc: "Once per week.",
-        img: "stronghold"
-    },
-    "ghost-ship": {
-        task: "Ghost Ship",
-        url: "https://lostarkive.com/guides/beginner/sea-activities/",
-        short: true,
-        desc: "At specific times.",
-        img: "ghost ship"
-    },
-    "pvp-island": {
-        task: "PVP Island",
-        url: "#",
-        short: true,
-        desc: "Determined by the occupying guild.",
-        img: "pvp island"
-    },
-    "chaos-line": {
-        task: "Chaos Line",
-        url: "#",
-        desc: "Twice per week",
-        img: "guardian raid"
-    },
-    "leaf-exchange": {
-        task: "Maharaka Leaf Exchange",
-        url: "#",
-        short: true,
-        desc: "Supply replenishes on weekly reset.",
-        img: "donation"
-    },
-    "illusion-token-exchange": {
-        task: "Winter Illusion Token Exchange",
-        url: "#",
-        short: true,
-        desc: "Supply replenishes on weekly reset.",
-        img: "winter"
-    }
-};
+   "guardian-challenge-mode":{
+      "task":"Guardian Challenge Mode 1",
+      "url":"https://papunika.com/guardian-subjugations/",
+      "desc":"1 per week per Boss (3 in total).",
+      "img":"guardian raid"
+   },
+   "guardian-challenge-mode-2":{
+      "task":"Guardian Challenge Mode 2",
+      "url":"https://papunika.com/guardian-subjugations/",
+      "desc":"1 per week per Boss (3 in total).",
+      "img":"guardian raid"
+   },
+   "guardian-challenge-mode-3":{
+      "task":"Guardian Challenge Mode 3",
+      "url":"https://papunika.com/guardian-subjugations/",
+      "desc":"1 per week per Boss (3 in total).",
+      "img":"guardian raid"
+   },
+   "challenge-abyss-dungeon":{
+      "task":"Challenge Abyss Dungeon",
+      "url":"https://papunika.com/abyss-dungeons/",
+      "short":true,
+      "desc":"1 per week per Challenge Abyss Dungeon.",
+      "img":"chal abyss dungeon"
+   },
+   "gvg-guild-boss":{
+      "task":"GVG / Guild Boss",
+      "url":"#",
+      "short":true,
+      "desc":"Once per week.",
+      "img":"guild"
+   },
+   "pvp-token-exchange":{
+      "task":"PVP Token Exchange",
+      "url":"#",
+      "short":true,
+      "desc":"Supply replenishes on weekly reset.",
+      "img":"pvp token"
+   },
+   "stronghold-activities":{
+      "task":"Stronghold Activities",
+      "url":"#",
+      "short":true,
+      "desc":"Once per week.",
+      "img":"stronghold"
+   },
+   "ghost-ship":{
+      "task":"Ghost Ship",
+      "url":"https://lostarkive.com/guides/beginner/sea-activities/",
+      "short":true,
+      "desc":"At specific times.",
+      "img":"ghost ship"
+   },
+   "pvp-island":{
+      "task":"PVP Island",
+      "url":"#",
+      "short":true,
+      "desc":"Determined by the occupying guild.",
+      "img":"pvp island"
+   },
+   "chaos-line":{
+      "task":"Chaos Line",
+      "url":"#",
+      "desc":"Twice per week",
+      "img":"guardian raid"
+   },
+   "Joytide Event Exchange":{
+      "task":"Joytide Event Exchange",
+      "url":"https://www.mtmmo.com/news/1044--lost-ark-festivity-island-guide-2022-date-rewards-quests-tips-of-lost-ark-winter-christmas-event",
+      "short":true,
+      "desc":"The Festivity Island has been added to Lost Ark in the December Update to hold the Christmas event and celebrate the holiday season with players.",
+      "img":"winter.png"
+   }
+}
